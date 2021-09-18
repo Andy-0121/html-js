a357a7fcc34c00bd5a27664d8ecc2dec89bbbdd2
梁煜昊Andy <andyliang@khkh.local>
2021-09-18 16:33:20 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("okokokok");
+alert("注意！");
+
+let confirm = confirm("请确定！")
+console.log(confirm);
+let input = prompt("嘿嘿！")
+console.log(input);
+
+
+let age = "17";
+let name =  "Alex";
+let isMan = true;
+
+console.log("你好" + name + "你今年" + age + "了");
